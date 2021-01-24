10b606aa464902ea08e2dcf4d1b415974c7a0363
titze <titze.hamburg@web.de>
2021-01-24 10:53:27 +0100

    ++ pid-Regler als Klasse implementiert

---
+++ service/pid_regler.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class PID_regler(object):
+
+    def __init__(self):
+        self.p_roll = 2.9
+        self.d_roll = 1.9
+        self.p_pitch = -1.9
+        self.d_pitch = -2
+
+    def innerLoopAileron(self, rollAngle_Reference, rollAngle_Current, rollAngleRateCurrent, AileronCurrent):
+        diff_rollAngle = rollAngle_Reference - rollAngle_Current
+        AileronCommand = diff_rollAngle * self.p_roll - rollAngleRateCurrent * self.d_roll
+        AileronCommand = AileronCommand + AileronCurrent
+        AileronCommand = np.clip(AileronCommand, -1, 1)
+        return AileronCommand
+
+    def innerLoopElevator(self, pitchAngleReference, pitchAngleCurrent, pitchAngleRateCurrent, elevatorCurrent):
+        errorPitchAngle = pitchAngleReference - pitchAngleCurrent
+        elevatorCommand = errorPitchAngle * self.p_pitch - pitchAngleRateCurrent * self.d_pitch
+        elevatorCommand = elevatorCommand + elevatorCurrent
+        elevatorCommand = np.clip(elevatorCommand, -1, 1) - 0.1
+        return elevatorCommand
--- tests/controller.py
+++ tests/controller.py
@@ -4,7 +4,10 @@ import pandas as pd
 from bokeh.plotting import figure, output_file, save
 from _datetime import datetime
 
+from service import pid_regler
+
 if __name__ == "__main__":
+    pid = pid_regler.PID_regler()
     df = pd.DataFrame(columns=['phi', 'u'])
     #todo: put to toml
 
@@ -25,26 +28,6 @@ if __name__ == "__main__":
         save(p)
 
 
-    p_roll = 2.9
-    d_roll = 1.9
-    p_pitch = -1.9
-    d_pitch = -2
-
-    # innerLoop: heading_roll->Aileron
-    def _innerLoopAileron(rollAngle_Reference, rollAngle_Current, rollAngleRateCurrent, AileronCurrent):
-        diff_rollAngle = rollAngle_Reference - rollAngle_Current
-        AileronCommand = diff_rollAngle * p_roll - rollAngleRateCurrent * d_roll
-        AileronCommand = AileronCommand + AileronCurrent
-        AileronCommand = np.clip(AileronCommand, -1, 1)
-        return AileronCommand
-
-    def _innerLoopElevator(pitchAngleReference, pitchAngleCurrent, pitchAngleRateCurrent, elevatorCurrent):
-        errorPitchAngle = pitchAngleReference - pitchAngleCurrent
-        elevatorCommand = errorPitchAngle * p_pitch - pitchAngleRateCurrent * d_pitch
-        elevatorCommand = elevatorCommand + elevatorCurrent
-        elevatorCommand = np.clip(elevatorCommand, -1, 1) - 0.1
-        return elevatorCommand
-
     sim = Simulation()
     result = sim.run()
     sim.set_controls('fcs/throttle-cmd-norm', 0)
@@ -55,10 +38,10 @@ if __name__ == "__main__":
         #print(i)
         state = sim.get_state()
         if i%5==0:
-            sim.set_controls('fcs/aileron-cmd-norm', _innerLoopAileron(np.deg2rad(10), state['phi'], state['p'],
+            sim.set_controls('fcs/aileron-cmd-norm', pid.innerLoopAileron(np.deg2rad(10), state['phi'], state['p'],
                                                                        sim.jsbsim.get_property_value(
                                                                            'fcs/aileron-cmd-norm')))
-            sim.set_controls('fcs/elevator-cmd-norm', _innerLoopElevator(np.deg2rad(-7), state['theta'], state['q'],
+            sim.set_controls('fcs/elevator-cmd-norm', pid.innerLoopElevator(np.deg2rad(-7), state['theta'], state['q'],
                                                                          sim.jsbsim.get_property_value(
                                                                              'fcs/elevator-cmd-norm')))
         sim.run()
