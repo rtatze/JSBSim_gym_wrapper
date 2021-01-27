b4c405ee56d3e71f4aba778009f2e5e74acf8df7
walter.laurito <walter.laurito@volunteerworld.com>
2021-01-27 12:14:01 +0100

    add setup.py for installation via pid, add gitignore

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(name='JSBSim_gym_wrapper',
+      version='0.1',
+      description='A simple open ai wrapper for jsbsim (flight dynamics model)',
+      url='https://github.com/rtatze/JSBSim_gym_wrapper',
+      author='Titze, Laurito',
+      license='MIT',
+      install_requires=[
+            'numpy',
+            'gym',
+            'matplotlib',
+            'jsbsim'
+      ],
+      packages=find_packages(),
+      classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Development Status :: 2 - Pre-Alpha',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      ],
+      python_requires='>=3.6',
+      include_package_data=True,
+      zip_safe=False)
